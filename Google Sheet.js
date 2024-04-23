45806a49c00ec0e76b4c24884f357223d8c605d7
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2024-04-23 12:29:41 +0000

    Add files via upload

---
+++ Google Sheet.js
@@ -0,0 +1,12 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwoDcTz3Ch9MR02Dgsl18F_Dt5ZPlgcrKB3-vMEJHvfDdnSIroAZU5k0ApqPuIiWJuZ2A/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! marks submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
