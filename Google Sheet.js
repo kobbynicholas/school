465112c391ba1aa9c1e21468de2a9f9b1140878a
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2024-06-27 14:21:31 +0000

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -3,7 +3,7 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwoDcTz3Ch9MR02Dgsl18
 
 const form = document.forms['contact-form']
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', f => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thank you! marks submitted successfully." ))
