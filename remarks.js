8b41f063e96c05b4f97a552d4b57beaad398e6a6
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2024-07-27 00:34:51 +0000

    Update remarks.js

--- remarks.js
+++ remarks.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', e => {
   e.preventDefault()
   
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! marks submitted successfully." ))
+  .then(response => alert("Thank you! Student remarks submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
