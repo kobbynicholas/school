d3f860d8e18fb55cb46507cf45e7186a80205b50
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2024-07-29 20:44:03 +0000

    Update remarks.js

--- remarks.js
+++ remarks.js
@@ -21,4 +21,4 @@ function hideLoader() {
 $(window).ready(hideLoader);
 
 // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
-setTimeout(hideLoader, 20 * 1000);
+setTimeout(hideLoader, 3 * 1000);
