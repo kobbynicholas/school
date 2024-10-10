b6ff952fe663061f828f6522c82d2dc0b898b0db
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2024-10-10 22:10:47 +0000

    Update Mock.js

--- Mock.js
+++ Mock.js
@@ -1,5 +1,5 @@
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx_2wzANmp0NLaH5DzNSALD6v7Z4uXNKmlsLUthevMVFjOEYzFz1f-h16BLPyZoZl9M/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyitxa8TRVTTAYSwSrP5stfqsPpLxNFqDooJyMu1coYJJJb0HUTQJE7eHLLcIkJmPyD/exec'
 
 const form = document.forms['contact-form']
  
