44755c2b2f51a5ae5e28fdfa4dbdd9677a822240
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2024-12-11 16:11:52 +0000

    Update exams.js

--- exams.js
+++ exams.js
@@ -1,17 +1,14 @@
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxXio7AwLywwyKKQTswAS8pd9AB26Xz_gWEtjH0p4VQpjT3JtjLtDVx7WLmHkBm0flyWg/exec'
-
 
 const form = document.forms['contact-form']
  
-form.addEventListener('submit', e => { 
+  form.addEventListener('submit', e => { 
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thank you! marks submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
-
 
 function showDiv() {
   document.getElementById('submit').style.display = "flex";
